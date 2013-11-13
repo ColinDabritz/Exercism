c4ae8e3587ad97b0e2124ce57a61ccdd0850c9c0
Colin Dabritz <Colin@dabritz.org>
2013-11-12 17:30:14 -0800

    fixed doc string comments

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -13,54 +13,58 @@ http://www.python.org/dev/peps/pep-0008/
 #  is important context. it must be below
 #  due to precedence issues
 
-""" class handling 'Bob' phrases
-"""
+
 class Bob(object):
-    """redirected because this has no
-       reason to be an instance method
-       structure forced by tests
+    """ class handling 'Bob' phrases
     """
+
     def hey(self, phrase):
+        """redirected because this has no
+           reason to be an instance method
+           structure forced by tests
+        """
         return hey(phrase)
 
 
-""" processes a 'Bob' phrase, giving the appropriate response
-"""
 def hey(phrase):
+    """ processes a 'Bob' phrase, giving the appropriate response
+    """
     # get the first response that passes the test for the phrase, in order
     return (response for (test,response) in RESPONSE_MAP if test(phrase)).next()
 
 
-""" determines if a particular phrase counts as a question
-"""
+
 def is_question(phrase):
+    """ determines if a particular phrase counts as a question
+    """
     # questions end with question marks
     return phrase.endswith("?")
 
 
-""" determines if a particular phrase is 'yelling'
-    yelling means all caps
-    (and has characters that change case)
-"""
 def is_yelling(phrase):
+    """ determines if a particular phrase is 'yelling'
+        yelling means all caps
+        (and has characters that change case)
+    """
     return (phrase == phrase.upper() # all upper case 
-        and phrase.lower() != phrase.upper() # and can change case
-        ) 
+    and phrase.lower() != phrase.upper() # and can change case
+    ) 
 
 
-""" determines if a phrase has no content
-    meaning empty or only whitespace
-"""
 def is_nothing(phrase):
+    """ determines if a phrase has no content
+        meaning empty or only whitespace
+    """
     # note: relies on 'empty strings are false' per pep8
     # http://www.python.org/dev/peps/pep-0008/
     # "For sequences, (strings, lists, tuples), 
     #   use the fact that empty sequences are false."
     return not phrase.strip() # strip whitespace and test for empty
 
-""" matches ANY phrase
-"""
+
 def is_anything_else(phrase):
+    """ matches ANY phrase
+    """
     return True
 
 """ note this is below function definitions due to 
