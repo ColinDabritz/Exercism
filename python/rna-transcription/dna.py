5fd9e3e1f21724baff27b48494c511130909c0cf
Colin Dabritz <Colin@dabritz.org>
2013-11-13 11:25:01 -0800

    better naming and clearer comments

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,12 +1,21 @@
 class DNA(object):
     '''Represents a strand of DNA'''
 
-    def __init__(self, base_code):
-        '''creates DNA for provided base code'''
-        self.code = base_code
+    def __init__(self, strand_nucleotide_sequence):
+        '''creates DNA for provided base code
+        note the strand is a string sequence of
+        DNA nucleotide letter codes
+        representing the coding sequence
+        (not the transcription sequence)
+        '''
+        self.strand = nucleotide_sequence
 
     def to_rna(self):
-        '''transcribes the DNA base code into RNA form'''
-        # base code transcription rules
-        # C = C, G = G, A = A, T = U
-        return self.code.replace('T','U')
+        '''transcribes the DNA coding sequence into RNA form
+        base code transcription rules (by code letter)
+        C = C,
+        G = G,
+        A = A,
+        T = U
+        '''
+        return self.strand.replace('T','U')
