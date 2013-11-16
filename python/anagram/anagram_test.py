8cf7c8e6c1b05761d53d1f9aa66afa468bbb4480
Colin Dabritz <Colin@dabritz.org>
2013-11-16 11:37:48 -0800

    first implementation of anagram

--- python/anagram/anagram_test.py
+++ python/anagram/anagram_test.py
@@ -1,5 +1,5 @@
 try:
-    from anagram import Anagram
+    from anagram import Anagram, cannonize
 except ImportError:
     raise SystemExit('Could not find anagram.py. Does it exist?')
 
@@ -59,6 +59,66 @@ class AnagramTests(unittest.TestCase):
             [],
             Anagram('banana').match(['banana'])
         )
+
+    # my tests
+    def test_cannonize_matches_same_twice(self):
+        self.assertEqual(
+            cannonize("word"),
+            cannonize("word"))
+
+    def test_cannonize_handles_case(self):
+        self.assertEqual(
+            cannonize("wOrd"),
+            cannonize("WoRd"))
+
+
+    def test_cannonize_ignores_punctuation(self):
+        self.assertEqual(
+            cannonize("it's"),
+            cannonize("its"))
+
+        self.assertEqual(
+            cannonize(")@(&#%)(@&#Compound-Word*@(#(&!)"),
+            cannonize("'(@@@)'Compou'ndWord&&&&&&&&&$$@()@#"))
+
+    def test_cannonize_repeated_letters(self):
+        self.assertNotEqual(
+            cannonize("aaaaab"),
+            cannonize("aab"))
+
+        self.assertEqual(
+            cannonize("aaab"),
+            cannonize("aaab"))
+
+    def test_cannonize_whitespace(self):
+        self.assertEqual(
+            cannonize("anagram"),
+            cannonize("nag a ram"))
+
+        self.assertEqual(
+            cannonize(" a   b   c     "),
+            cannonize("abc"))
+
+        self.assertEqual(
+            cannonize("""a
+            b
+            c d   e   f     
+            """),
+            cannonize("abcdef"))
+
+
+    def test_is_anagram_no_match_to_self(self):
+        self.assertFalse(Anagram("word").is_anagram("word"))
+
+    def test_is_anagram_matches_jumble(self):
+        self.assertTrue(Anagram("word").is_anagram("rowd"))
+
+    def test_is_anagram_matches_classic_phrase(self):
+        self.assertTrue(Anagram("anagram").is_anagram("nag a ram"))
+
+    def test_is_anagram_no_match(self):
+        self.assertFalse(Anagram("one").is_anagram("two"))
+
 
 if __name__ == '__main__':
     unittest.main()
